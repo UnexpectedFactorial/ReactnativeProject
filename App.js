1f08d0f61c9f64f6cd0adf05448c2d5bf3148f42
UnexpectedFactorial <chengkevin34@gmail.com>
2023-07-24 10:20:01 +0000

    Completed location code and reverse geocode

--- App.js
+++ App.js
@@ -1,44 +1,56 @@
-import {Platform, Text, View, StyleSheet } from 'react-native';
+import { Text, View, StyleSheet, AcivityIndicator, Alert, SafeAreaView, ScrollView, RefreshControl } from 'react-native';
 import Constants from 'expo-constants';
-import {useState , useEffect} from 'react';
-import React from 'react';
-import * as Location from 'expo-location';
-import MapView from 'react-native-maps';
+import  * as Location from 'expo-location';
+import {useState, useEffect} from 'react';
 
 export default function App() {
-  const [initialLocation, setInitialLocation] = useState(null);
-  const [longitude, setLongitude] = useState(null);
-  const [latitude,setLatitude] = useState(null);
+  const [rGeocode, setRGeo] = useState();
+  const [location, setLocation] = useState();
+  const [currentLatitude, setLatitude] = useState();
+  const [currentLongitude, setLongitude] = useState();
+  const [currentCity, setCity] = useState();
+  const [forecast, setForecast] = useState(null);
+  const [response, setResponse] = useState(false);
 
+  const Weather_API = 'e4526a4bfd4f09fe6ff15be0585a6e41';
+  let url = 'http://api.weatherapi.com/v1/forecast/json?key=${Weather_API}&q=${location}&days=1&aqi=no&alerts=no';
 
-  useEffect(() => {
-    const getPermissions = async () => {
+  useEffect(()=>{
+    const getPermissions = async () =>{
       let {status} = await Location.requestForegroundPermissionsAsync();
-      if(status !== 'granted'){
-        console.log("No Permissions");
+      if (status !== 'granted'){
         return;
       }
 
-      let location = await Location.getCurrentPositionAsync({});
-      setLatitude(location.coords.latitude);
-      setLongitude(location.coords.longitude);
+      let currentLocation = await Location.getCurrentPositionAsync({});
+      setLocation(currentLocation);
+      console.log(currentLocation);
 
-      //grab latitude and longitude values with {latitude} and {longitude} respectively
+      setLatitude(currentLocation.coords.latitude);
+      setLongitude(currentLocation.coords.longitude);
+
+      let reverseGeocoding = await Location.reverseGeocodeAsync({
+        latitude: currentLocation.coords.latitude,
+        longitude: currentLocation.coords.longitude,
+      })
+      setCity(reverseGeocoding[0].city);
+
     };
     getPermissions();
-  },[]);
-
-  
+  },[])
 
   return (
     <View style={styles.container}>
-        <Text>
-          {latitude}
-          {longitude}
-        </Text>
+      <Text style={styles.paragraph}>
+        You are currently at:
+      </Text>
+      <Text style={styles.paragraph}> {currentLatitude} {"\n"} {currentLongitude} {"\n"} {currentCity}
+      </Text>
     </View>
-  );
+    );
 }
+
+const current = forecast.current.weather[0];
 
 const styles = StyleSheet.create({
   container: {
@@ -47,5 +59,11 @@ const styles = StyleSheet.create({
     paddingTop: Constants.statusBarHeight,
     backgroundColor: '#ecf0f1',
     padding: 8,
+  },
+  paragraph: {
+    margin: 24,
+    fontSize: 18,
+    fontWeight: 'bold',
+    textAlign: 'center',
   },
 });
